1acadd88f44e3079e8cf13104531c6b6fe442f86
Tom Coleman <tom@thesnail.org>
2013-11-26 19:55:15 +1100

    Added active classes to the header.
    chapter13-6

--- client/views/includes/header.js
+++ client/views/includes/header.js
@@ -1,3 +1,13 @@
 Template.header.helpers({
-  routeOptions: {limit: 3}
+  routeOptions: {limit: 3},
+  activeRouteClass: function(/* route names */) {
+    var args = Array.prototype.slice.call(arguments, 0);
+    args.pop();
+    
+    var active = _.any(args, function(name) {
+      return Router.current().route.name === name
+    });
+    
+    return active && 'active';
+  }
 });
