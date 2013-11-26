edd77688149914928dc03e2a60f258e099524602
Tom Coleman <tom@thesnail.org>
2013-11-26 19:57:08 +1100

    Added post reordering animation.
    chapter14-1

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,4 +1,10 @@
 Template.postsList.helpers({
+  postsWithRank: function() {
+    return this.map(function(post, index, cursor) {
+      post._rank = index;
+      return post;
+    });
+  },
   hasMorePosts: function(){
     // as long as we ask for N posts and all N posts showed up, then keep showing the "load more" button
     return Router.current().limit() == this.count();
