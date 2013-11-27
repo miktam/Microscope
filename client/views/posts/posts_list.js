89ba55bfde33c2ad76fe4f8068ebb7f394c55ff5
Tom Coleman <tom@thesnail.org>
2013-11-27 12:52:32 +1100

    Factored data out

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,16 +1,13 @@
 Template.postsList.helpers({
   postsWithRank: function() {
-    this.rewind();
-    return this.map(function(post, index, cursor) {
+    this.posts.rewind();
+    return this.posts.map(function(post, index, cursor) {
       post._rank = index;
       return post;
     });
   },
   hasMorePosts: function(){
     // as long as we ask for N posts and all N posts showed up, then keep showing the "load more" button
-    return Router.current().limit() == this.count();
-  },
-  loadMorePath: function () {
-    return Router.current().nextPath();
+    return Router.current().limit() == this.posts.count();
   }
 });
