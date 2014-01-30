0e643ec06801a85e526083bf7c062c35b74652e2
Tom Coleman <tom@thesnail.org>
2014-01-30 14:10:31 +1100

    Fix limit bug

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,13 +1,8 @@
 Template.postsList.helpers({
   postsWithRank: function() {
-    this.posts.rewind();
     return this.posts.map(function(post, index, cursor) {
       post._rank = index;
       return post;
     });
-  },
-  hasMorePosts: function(){
-    this.posts.rewind();
-    return Router.current().limit() == this.posts.fetch().length;
   }
 });
